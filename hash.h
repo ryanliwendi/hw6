679480c3d607eaa552a98e0883dc40492594480a
ryanliwendi <liryan@usc.edu>
2025-05-05 15:38:48 -0700

    Updated String hash function

--- hash.h
+++ hash.h
@@ -20,15 +20,53 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
-
-
+        int digits[28];
+        for (int i = 0; i < 28; i++)
+        {
+            digits[i] = 0;
+        }
+        for (int i = 0; i < k.size(); i++)
+        {
+            int pos = 28 - k.size() + i;
+            digits[pos] = letterDigitToNumber(k[i]);
+        }
+        unsigned long long w[5];
+        int start = 22, end = 27;
+        int curpos = 4;
+        while (end > 0)
+        {
+            int len = end - start + 1;
+            unsigned long long num = 0;
+            for (int i = 0; i < len; i++)
+            {
+                num *= 36;
+                num += digits[start + i];
+            }
+            w[curpos] = num;
+            curpos --;
+            start -= 6;
+            end -= 6;
+            if (start < 0)
+                start = 0;
+        }
+        unsigned long long ans = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            ans += w[i] * rValues[i];
+        }
+        return ans;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
-
+        if (letter >= '0' && letter <= '9')
+        {
+            return letter - '0' + 26;
+        }
+        letter = std::tolower(letter);
+            return letter - 'a';
     }
 
     // Code to generate the random R values
